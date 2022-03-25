f453bac314fc93177beacc7ed8c5911777248b5c
Ma161ric <ma161ric@htwg-konstanz.de>
2022-03-25 11:39:36 +0100

    gitignore, stringausgabe

--- src/main/scala/shithead.scala
+++ src/main/scala/shithead.scala
@@ -1,8 +1,23 @@
+package main.scala
 import scala.compiletime.ops.int
-@main
-def run:Unit = karte
-def eol = "\n"
-def anzahlKarten = 5
-def karte = for (i <- 1 to anzahlKarten){
-    println("|" + i + "|" + eol)
-}
+import scala.io.StdIn._
+
+@main def run: Unit =
+    println("Willkommen zu Shithead!\n")
+
+    println("Anzahl Karten eingeben:")
+    var anzKarten = readInt()
+    while (anzKarten < 1 || anzKarten > 9) {
+    println("Falscher Input. Bitte neu eingeben:")
+    anzKarten = readInt()
+  }
+
+    alleKarten(anzKarten)
+    def alleKarten(x:Int) = "" + topBar(x) + zahlen(x) + subBar(x)
+    def eol = sys.props("line.separator")
+    def bar = "+-+ "
+    def topBar(x:Int) = print(bar * x + eol)
+    def subBar(x:Int) = print(eol + bar * x + eol)
+    def zahlen(x:Int) = for (i <- 1 to x){
+        print("|" + i + "| ")
+    }
--- src/main/scala/shithead.worksheet.sc
+++ src/main/scala/shithead.worksheet.sc
@@ -1,2 +1,12 @@
 1+1
 2*3
+
+def alleKarten(x:Int) = "" + topBar(x) + zahlen(x) + subBar(x)
+def eol = "\n"
+def bar = "+-+ "
+def topBar(x:Int) = print(bar * x + eol)
+def subBar(x:Int) = print(eol + bar * x + eol)
+def zahlen(x:Int) = for (i <- 1 to x){
+    print("|" + i + "| ")
+}
+println(alleKarten(9))
